84190aaa2dd4d7805ab6d3f62cfe7f8cfd07959e
stefanandonian <stefan.andonian@gmail.com>
2016-10-02 12:56:56 +0200

    Added Image Banner to Homepage

--- app/src/main/java/net/conciencia/mensajeandroid/HomePageActivity.java
+++ app/src/main/java/net/conciencia/mensajeandroid/HomePageActivity.java
@@ -4,19 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class SermonListActivity extends AppCompatActivity implements SermonListFragment.SermonListInteraction {
+public class HomePageActivity extends AppCompatActivity implements SermonListFragment.SermonListInteraction {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sermon_list);
+        setContentView(R.layout.activity_homepage);
     }
 
     @Override
     public void onSermonSelected(RSSInterface rssInterface, int sermonIndex) {
         //todo
         //Toast.makeText(this, String.format("Sermon %s (%s) selected", sermon.getTitle(), sermon.getPubDate()), Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(SermonListActivity.this, SermonViewActivity.class);
+        Intent intent = new Intent(HomePageActivity.this, SermonViewActivity.class);
         Bundle extras = new Bundle();
         extras.putParcelable(Constants.RSS_EXTRA, rssInterface);
         extras.putInt(Constants.SERMON_INDEX, sermonIndex);
